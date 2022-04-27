10e7720a19a97248ea94f7b1923aecf02ae3adff
AA <aa@AAs-MacBook-Air.local>
2022-04-27 19:33:17 +0300

    seasons

--- src/get-email-domain.js
+++ src/get-email-domain.js
@@ -10,9 +10,11 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getEmailDomain(email) {
+  let s=email.split("").reverse().join("");
+  let k=s.indexOf('@');
+  s=s.slice(0,k);
+  return s.split("").reverse().join("");
 }
 
 module.exports = {
