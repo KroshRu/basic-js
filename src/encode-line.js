a82e6d86e6e3111ae1d04fedc189c7bbe6bcc00e
AA <aa@AAs-MacBook-Air.local>
2022-04-27 19:17:22 +0300

    seasons

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let res='';
+  for (let i=0;i<str.length;i++){
+    let k=1;
+    if (str[i]==str[i+1]){
+       k++;
+       i++;
+       while (str[i]==str[i+1])
+       {k++;i++;};
+       res+=k+str[i];
+    }
+    else {res+=str[i];}
+  }
+  return res;
 }
 
 module.exports = {
